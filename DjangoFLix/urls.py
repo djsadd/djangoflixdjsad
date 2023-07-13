8cc4b3231376d703ce71080c0c0cd67967673bc0
Erasil <erasil.kazah222@gmail.com>
2023-07-13 17:18:38 +0600

    update

--- DjangoFLix/urls.py
+++ DjangoFLix/urls.py
@@ -19,6 +19,8 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("playlists.urls")),
@@ -26,5 +28,5 @@ urlpatterns = [
     path('', include('users.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
